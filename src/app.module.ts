dc13fc9391b2a327a7dbadb28b356efa388bf02a
ngoduongkha <ngoduongkhakg2001@gmail.com>
2022-07-25 22:20:00 +0700

    something

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    PrismaModule,
+    BookmarkModule,
+    UserModule,
+  ],
+})
+export class AppModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,75 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+  async signIn(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Incorrect email or password');
+    }
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) {
+      throw new ForbiddenException('Incorrect email or password');
+    }
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signUp(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+    }
+  }
+
+  async signToken(userId: number, email: string): Promise<{ token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const JWT_TOKEN_SECRET = this.config.get('JWT_TOKEN_SECRET');
+    const JWT_TOKEN_LIFE = this.config.get('JWT_TOKEN_LIFE');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: JWT_TOKEN_LIFE,
+      secret: JWT_TOKEN_SECRET,
+    });
+
+    return {
+      token,
+    };
+  }
+}
